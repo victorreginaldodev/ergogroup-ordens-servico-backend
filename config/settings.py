8d2ecaa1ebaf9f85de8d898c9efaecb21877a7c6
Dev Victor <contato.victordev02@gmail.com>
2026-04-28 09:02:16 -0300

    add app legado aos installed apps para migração de dados

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
     'corsheaders',
+    # App legado para migração de dados
+    'legado.ordemServico',
 ]
 
 AUTH_USER_MODEL = 'contas.Usuario'
